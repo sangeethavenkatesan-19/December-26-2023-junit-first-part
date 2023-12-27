e32ce3f959c81f301da25e065bd3f838b1749b5f
sangeethavenkatesan-19 <sangeethahemavenkat@gmail.com>
2023-12-27 17:46:54 +0530

    First commit

---
+++ src/main/java/Phase2_JUNIT/Monday/App.java
@@ -0,0 +1,13 @@
+package Phase2_JUNIT.Monday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
